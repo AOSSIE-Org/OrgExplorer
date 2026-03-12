fb995e5e1c4601f7e6a67d80826ccd8792795008
Kunal Bunkar <kunalbunkar0104@gmail.com>
2026-03-12 12:37:06 +0530

    make 404 page in seprate file

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,22 @@
 import './App.css'
-import { Link, Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
+import NotFoundPage from './NotFoundPage'
+
+function HomeRoute() {
+
+  return (
+    <>
+      <h1>Hello, OrgExplorer!</h1>
+    </>
+  )
+}
 
 function App() {
-  function HomeRoute() {
-
-    return (
-      <>
-        <h1>Hello, OrgExplorer!</h1>
-      </>
-    )
-  }
-
-
-
-  function NotFoundRoute() {
-    return (
-      <>
-        <h1>404</h1>
-        <p>Page not found.</p>
-        <Link to="/">Go home</Link>
-      </>
-    )
-  }
-
   return (
     <>
       <Routes>
         <Route path="/" element={<HomeRoute />} />
-        <Route path="*" element={<NotFoundRoute />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </>
   )
