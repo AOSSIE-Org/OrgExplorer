cfb582bef8bcdc263e96677590c2f4beee0b8f6b
delta student <someone@email.com>
2026-03-18 21:53:03 +0530

    feat: add GitHub token configuration button

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,10 @@ function App() {
   return (
     <>
       <h1>Hello, OrgExplorer!</h1>
+      {/*Adding the button here */}
+      <button>
+        Configure Github Token
+      </button>
     </>
   )
 }
