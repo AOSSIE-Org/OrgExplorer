be44492ae239b8b657e3bbc88f7a11b806fab381
Coderpurohit <bharatpurohit095@gmail.com>
2026-05-13 18:57:07 +0530

    Add React Router support with 404 page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
-import './App.css'
+import { Routes, Route } from 'react-router-dom'
+
+function Home() {
+  return (
+    <h1>Hello, OrgExplorer!</h1>
+  )
+}
+
+function NotFoundPage() {
+  return (
+    <div>
+      <h1>404 - Page Not Found</h1>
+      <a href="/">Go Back Home</a>
+    </div>
+  )
+}
 
 function App() {
-
   return (
-    <>
-      <h1>Hello, OrgExplorer!</h1>
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
   )
 }
 
